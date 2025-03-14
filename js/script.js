640ed6e503c6c41d2c403234e04836f921ac6ffe
maysha29 <mayshatabassum29@gmail.com>
2025-03-14 15:50:56 +0600

    added the quantitiy part

--- js/script.js
+++ js/script.js
@@ -8,6 +8,38 @@ for (const button of ringButtons) {
       event.target.classList.add("border-purple-600")
       event.target.classList.remove("border-gray-300")
 
+      const image=document.getElementById("product-image")
+      const color=event.target.id.split('-color')[0];
+      const imageBase="../images/";
+      image.src=`${imageBase}${color}.png`;
   })
-  
 }
+  const sizes=['S','M','XL','L']
+  for (const size of sizes) {
+    const sizebtn=document.getElementById(`size-${size}`);
+  sizebtn.addEventListener('click', function () {
+    for (const sizeClick of sizes) {
+      const sizeButton = document.getElementById(`size-${sizeClick}`);
+      sizeButton.classList.remove('border-purple-700')
+    }
+    sizebtn.classList.add('border-purple-700')
+  })
+
+let quantity = 0
+const quantityButtons = document.querySelectorAll(".quantity-button");
+for (const btn of quantityButtons) {
+  btn.addEventListener('click', function (event) {
+    if (event.target.innerText === '+') {
+      quantity++
+    }
+    else if (event.target.innerText === '-') {
+      quantity--
+    }
+     if (quantity < 0) {
+      quantity = 0;
+    }
+
+  document.getElementById("quantity").innerText = quantity
+  })
+}
+  }
